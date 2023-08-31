ff65e5bdedfef2a7a15ee212c8a7ae318ca70e1d
CodWave <143485390+CodeWave9@users.noreply.github.com>
2023-08-31 19:34:29 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Netflix from "./Netflix";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Netflix />);
